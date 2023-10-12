712a914df15916efe3d78bccb53192f1c58f4d64
Andrew Marabante <andrewmarabante@gmail.com>
2023-10-12 12:01:01 -0700

    Added tests for ship class. Made an objects.js to bypass webpack workarounds for jest (Ran into error when trying to test in index.js)

--- src/objects.js
+++ src/objects.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 class ship 
 {
   constructor(length, numhits = 0, sunk = false)
@@ -10,14 +8,10 @@ class ship
   }
   hit()
   {
-    console.log('working1');
   }
   isSunk()
   {
-    console.log('working2');
   }
 }
 
-let ship1 = new ship(3);
-console.log(ship1.isSunk);
-console.log(ship1.sunk);
+module.exports = ship;
---
+++ src/ship.test.js
@@ -0,0 +1,42 @@
+const { constant } = require('lodash');
+const ship = require('./objects');
+
+test('Ship has a default length of 1',() => {
+  const ship1 = new ship();
+  expect(ship1.length).toBe(1);
+});
+
+test('Ship can take in different lengths', () => {
+  const ship1 = new ship(2);
+  const ship2 = new ship(1);
+  const ship3 = new ship(3);
+  expect(ship1.length).toBe(2);
+  expect(ship2.length).toBe(1);
+  expect(ship3.length).toBe(3);
+});
+
+test('Ship starts with 0 numhits by default',() => {
+  const ship1 = new ship();
+  expect(ship1.numhits).toBe(0);
+});
+
+test('Ship starts with sunk = false by default',() => {
+  const ship1 = new ship();
+  expect(ship1.sunk).toBe(false);
+});
+
+
+test('hit function adds 1 to numhits',() => {
+  const ship1 = new ship();
+  ship1.hit();
+  ship1.hit();
+  ship1.hit();
+  expect(ship1.numhits).toBe(3);
+});
+
+test('isSunk checks if numhits = length, if so turns sunk from false to true',() => {
+  const ship1 = new ship();
+  expect(ship1.isSunk).toBe(false);
+  ship1.hit();
+  expect(ship1.isSunk).toBe(true);
+});
