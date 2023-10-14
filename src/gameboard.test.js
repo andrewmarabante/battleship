24bdb98f0940c08a2e3ee323c831f3fcdc63d705
Andrew Marabante <andrewmarabante@gmail.com>
2023-10-14 15:13:40 -0700

    Passed the first tests on gameboard

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,22 +1,30 @@
 const {board} = require('./objects');
+const {ship} = require('./objects');
 
 test('Can place ships at specific coordinates',() => {
     const board1 = new board;
-    board1.placeShip(ship3,[5,6]);
-    expect(board1.ship3).toBe([[5,6],[5,7],[5,8]])
+    const ship3 = new ship(3,[[3,1],[3,2],[3,3]]);
+    board1.placeShip(ship3,[1,2]);
+    expect(ship3.coord).toEqual([[1,2],[1,3],[1,4]])
   });
 
 test('Takes an attack,determines hit or miss, and records it',() => {
     const board1 = new board;
-    board1.receiveAttack([9,9]);
-    expect(board1.miss).toBe([9,9])
-    board1.receiveAttack([1,1]);
-    expect(board1.hit).toBe([1,1])
+    board1.setBoard();
+    board1.receiveAttack([5,2]);
+    board1.receiveAttack([4,2]);
+    board1.receiveAttack([1,2]);
+    board1.receiveAttack([6,2]);
+    board1.receiveAttack([8,2]);
+    board1.receiveAttack([9,3]);
+    expect(board1.hit).toEqual([[5,2],[4,2],[6,2]]);
+    expect(board1.miss).toEqual([[1,2],[8,2],[9,3]]);
   });
 
 test('Sends a hit to corresponding ship if hit',() => {
     const board1 = new board;
-    board1.receiveAttack([1,1]);
-    expect(ship1.numhits).toBe(1);
-    expect(ship1.sunk).toBe(true);
+    board1.setBoard;
+    board1.receiveAttack([3,1]);
+    board1.receiveAttack([3,2]);
+    expect(ship3.numhits).toBe(2);
   });
--- src/objects.js
+++ src/objects.js
@@ -1,10 +1,11 @@
 class ship 
 {
-  constructor(length = 1, numhits = 0, sunk = false)
+  constructor(length = 1, coord = [])
   {
     this.length = length;
-    this.numhits = numhits;
-    this.sunk = sunk;
+    this.numhits = 0;
+    this.sunk = false;
+    this.coord = coord;
   }
   hit()
   {
@@ -25,22 +26,94 @@ class gameboard
   {
     this.miss = [];
     this.hit = [];
-    this.ship1 = [[1,1]];
-    this.ship2 = [[2,1],[2,2]];
-    this.ship3 = [[3,1],[3,2],[3,3]];
-    this.ship4 = [[4,1],[4,2],[4,3]];
-    this.ship5 = [[5,1],[5,2],[5,3],[5,4]];
-    this.ship6 = [[6,1],[6,2],[6,3],[6,4],[6,5]];
   }
 
-    placeShip()
+    setBoard()
     {
-
+      globalThis.ship1 = new ship(1,[[1,1]]);
+      globalThis.ship2 = new ship(2,[[2,1],[2,2]]);
+      globalThis.ship3 = new ship(3,[[3,1],[3,2],[3,3]]);
+      globalThis.ship4 = new ship(3,[[4,1],[4,2],[4,3]]);
+      globalThis.ship5 = new ship(4,[[5,1],[5,2],[5,3],[5,4]]);
+      globalThis.ship6 = new ship(5,[[6,1],[6,2],[6,3],[6,4],[6,5]]);
     }
 
-    receiveAttack()
+    placeShip(ship,[x,y])
     {
+      ship.coord=[];
+      for(let i=0;i< ship.length;i++)
+      {
+        let newcoord = [x,y+i];
+        ship.coord.push(newcoord);
+      }
+    }
 
+    receiveAttack([x,y])
+    {
+      let hitconf = false;
+
+      //ship1
+      if(ship1.coord[0][0] === x && ship1.coord[0][1] === y)
+      {
+        this.hit.push([x,y]);
+        hitconf = true;
+        ship1.hit();
+      }
+      //ship2
+        for(let i=0;i<=1;i++)
+        {
+            if(ship2.coord[i][0] === x && ship2.coord[i][1] ===y)
+            {
+              this.hit.push([x,y]);
+              hitconf = true;
+              ship2.hit();
+            }
+        }
+        //ship3
+        for(let i=0;i<=2;i++)
+        {
+            if(ship3.coord[i][0] === x && ship3.coord[i][1] ===y)
+            {
+              this.hit.push([x,y]);
+              hitconf = true;
+              ship3.hit();
+            }
+        }
+        //ship4
+        for(let i=0;i<=2;i++)
+        {
+            if(ship4.coord[i][0] === x && ship4.coord[i][1] ===y)
+            {
+              this.hit.push([x,y]);
+              hitconf = true;
+              ship4.hit();
+            }
+        }
+        //ship5
+        for(let i=0;i<=3;i++)
+        {
+            if(ship5.coord[i][0] === x && ship5.coord[i][1] ===y)
+            {
+              this.hit.push([x,y]);
+              hitconf = true;
+              ship5.hit();
+            }
+        }
+        //ship6
+        for(let i=0;i<=4;i++)
+        {
+            if(ship6.coord[i][0] === x && ship6.coord[i][1] ===y)
+            {
+              this.hit.push([x,y]);
+              hitconf = true;
+              ship6.hit();
+            }
+        }
+
+      if(hitconf === false)
+      {
+        this.miss.push([x,y]);
+      }
     }
 
     checkGame()
@@ -49,13 +122,6 @@ class gameboard
     }
 }
 
-
-  const ship1 = new ship(1);
-  const ship2 = new ship(2);
-  const ship3 = new ship(3);
-  const ship4 = new ship(3);
-  const ship5 = new ship(4);
-  const ship6 = new ship(5);
 
 exports.board = gameboard;
 exports.ship = ship;
